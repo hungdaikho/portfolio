69eb13466b66d5d05b05c1ddbe4b0966c2dbe13a
hungdeptrai <hungdeptraiquachoi@gmail.com>
2023-11-01 23:08:29 +0700

    release

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import styles from "./Main.module.scss";
 const Main = () => {
-  return <div className={styles.main}>Main</div>;
+  return (
+    <div className={styles.main}>
+      <div className="title">
+        <img src="/images/thang.png" alt="" />
+        <span>projects</span>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
