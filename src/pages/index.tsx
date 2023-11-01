7e9333489e7d8a79a8e345f25f6c93dc61f58109
hungdeptrai <hungdeptraiquachoi@gmail.com>
2023-11-01 23:39:38 +0700

    add multiple theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,11 @@
-import CanvasAction from "@/components/CanvasAction/CanvasAction";
-import Header from "@/components/Header/Header";
-import Main from "@/components/Main/Main";
-import WelCome from "@/components/WelCome/WelCome";
-import { Button } from "antd";
 import React from "react";
 
 type Props = {};
 
 const index = ({}: Props) => {
   return (
-    <div className="landingPage">
-      <title>Tran Van Hung</title>
-      <div className="network">
-        <img src="/images/line.png" alt="" />
-        <Button
-          className="btn"
-          icon={<img src="/images/github.png" alt="" />}
-        />
-        <Button
-          className="btn"
-          icon={<img src="/images/network.png" alt="" />}
-        />
-      </div>
-      <div className="container">
-        {/* <CanvasAction /> */}
-        <Header />
-        <WelCome />
-        <Main />
-      </div>
+    <div className="landingPage" id="themeBoundary">
+      <button>CHANGE THEME</button>
     </div>
   );
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,21 +1,14 @@
-import { menuSlice } from '@/reducers/MenuReducer';
-import {
-    Action,
-    configureStore,
-    ThunkAction,
-  } from '@reduxjs/toolkit';
-  
-  export const store = configureStore({
-    reducer: {
-        menu: menuSlice.reducer
-    },
-  });
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
 
-  export type AppDispatch = typeof store.dispatch;
-  export type RootState = ReturnType<typeof store.getState>;
-  export type AppThunk<ReturnType = void> = ThunkAction<
-     ReturnType,
-     RootState,
-     unknown,
-     Action<string>
-   >;
+export const store = configureStore({
+  reducer: {},
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
