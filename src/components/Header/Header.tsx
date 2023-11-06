0fc82542943e7f4621cf897be713e1c4e4e39150
hungdeptrai <hungdeptraiquachoi@gmail.com>
2023-11-06 21:38:54 +0700

    av

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import styles from "./Header.module.scss";
 type Props = {};
 const Header = ({}: Props) => {
-  return <div className={styles.header}>Header</div>;
+  return (
+    <div className={styles.header}>
+      <div className={styles.logo}>Van Hung T.</div>
+      <div className={styles.menu}>ABC</div>
+    </div>
+  );
 };
 
 export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import Header from "@/components/header/Header";
 import { changeTheme } from "@/utils/theme";
 import React from "react";
 type Props = {};
 const index = ({}: Props) => {
   return (
     <div className="landingPage" id="themeBoundary">
+      <Header />
       <button
         id="themeBtn"
         onClick={() => {
