72726b4c38c5144646aee731e7ea354377cfcc5d
Hung Dep Trai <tranvanhungvx@gmail.com>
2023-11-09 00:35:01 +0700

    f

--- src/components/Service/Service.tsx
+++ src/components/Service/Service.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import styles from "./Service.module.scss";
+import { title } from "process";
 type Props = {};
 
 const Service = ({}: Props) => {
   return (
     <div className={styles.service}>
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        <div className={styles.title}>What I Do ?</div>
+      </div>
     </div>
   );
 };
