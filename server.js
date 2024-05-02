d3a0ec4f733383d43545b77007313d33e888f13d
Hung Dep Trai <tranvanhungvx@gmail.com>
2024-05-02 12:31:45 +0700

    add ssl

---
+++ server.js
@@ -0,0 +1,24 @@
+// server.js
+const https = require("https");
+const fs = require("fs");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const httpsOptions = {
+  key: fs.readFileSync("/etc/letsencrypt/live/hungsayqua.id.vn/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/hungsayqua.id.vn/fullchain.pem"),
+};
+
+app.prepare().then(() => {
+  https
+    .createServer(httpsOptions, (req, res) => {
+      handle(req, res);
+    })
+    .listen(443, (err) => {
+      if (err) throw err;
+      console.log("> Ready on https://localhost:443");
+    });
+});
